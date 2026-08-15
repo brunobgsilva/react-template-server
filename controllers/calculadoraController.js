8d0d36008354728cc3ef6fe246ae53902fe7ffb7
bruno <bruno_bg_silva@estudante.sesisenai.org.br>
2026-08-14 21:46:52 -0300

    feat: implementar calculo de raizes nao exatas

--- controllers/calculadoraController.js
+++ controllers/calculadoraController.js
@@ -1,4 +1,4 @@
-exports.calcular = (req, res) => {
+exports.calcular = async (req, res) => {
     const body = req.body;
 
     const { num1, num2, operation } = body;
@@ -17,5 +17,28 @@ exports.calcular = (req, res) => {
         res.status(200).json({result: num1 * num2});
     } else if (operation === "/") {
         res.status(200).json({result: num1 / num2});
-    } 
+    } else if (operation === "sqrt") {
+        const sqrtResult = calcSqrt(num1);
+        res.status(200).json({result: sqrtResult});
+    };
 };
+
+function calcSqrt(num) {
+    let result;
+    let foundSqrt = false;
+    let i = 1;
+
+    while (!foundSqrt) {
+        if (i * i == num) {
+            foundSqrt = true;
+            result = i;
+        } else if (i * i < num) {
+            i += 1;
+        } else if (i * i > num) {
+            result = "Raiz não exata.";
+            break;
+        };
+    };
+
+    return result;
+}
